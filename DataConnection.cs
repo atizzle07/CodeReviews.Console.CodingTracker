72bf9ad56d8e8a655774d2ece6215fd4f0e2d7d5
Allen Tripp <13400103+atizzle07@users.noreply.github.com>
2025-12-09 01:16:29 -0600

    Initial Commit. Created Data connection and event class

---
+++ DataConnection.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Text;
+using System.Configuration;
+
+
+namespace CodingTrackerApp;
+
+public class DataConnection
+{
+    private static string databaseName = "codingtracker";
+    string tableName = "event";
+    static string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+    public DataConnection()
+    {
+        CreateTable();
+    }
+    public void CreateTable()
+    {
+        string createTableText = @$"CREATE TABLE IF NOT EXISTS {tableName}(
+                                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                StartTime TEXT,
+                                Endtime TEXT,
+                                Details TEXT
+                                )";
+
+        using (var conn = new SQLiteConnection(connectionString))
+        {
+            conn.Execute(createTableText);
+        }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTrackerApp.Models;
+
+internal class Event
+{
+    public int ID { get; set; }
+    public required string StartTime { get; set; }
+    public string? EndTime { get; set; }
+    public string? Details { get; set; }
+}
